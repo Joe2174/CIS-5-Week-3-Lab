d1d686941bf5717f8657711a13372151169f2f03
Joe2174 <jbarron36@student.rccd.edu>
2026-09-04 11:55:58 -0700

    Work in progress.
    Checkpoint:

--- main.cpp
+++ main.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
 #include <string>
 
+using std::cout;
+using std::cin;
+using std::string;
+
 // Lab 3 — Your Name
 // CIS 5 Week 03 · Types & variables
 
 int main() {
-  const int CURRENT_YEAR = 2026;
+	string name;
+	cout << "Name: ";
+	cin >> name;
+	int Age = 0;
+	cout << "Age: ";
+	cin >> Age;
+	double height_m = 0.0;
+	cout << "Height (m): ";
+	cin >> height_m;
+	char initial;
+	cout << "Initial: ";
+	cin >> initial;
+	bool student;
+	cout << "Student: ";
+	cin >> student;
+	int CURRENT_YEAR = 2026;
+	cin >> CURRENT_YEAR;
+	cout << "Current Year is: " << CURRENT_YEAR << "\n";
 
   // TODO: declare + initialize
   // std::string name = ...;
@@ -14,7 +35,7 @@ int main() {
   // char initial = ...;
   // bool student = ...;
 
-  std::cout << "=== About me ===\n";
+  cout << "==== About me ====\n";
   // TODO: labeled prints for every variable + CURRENT_YEAR
 
   return 0;
